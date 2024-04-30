01fc95632f513883c4c7c22e0873ca80d957b592
Pratham Kumar <kumarp@terralink-global.com>
2024-04-30 09:51:56 +0000

    First Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/kpo/manageconagreement/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comkpo/manageconagreement/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
